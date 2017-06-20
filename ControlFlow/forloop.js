b897eff909038c4e90107f872709e670ae26c24c
Brooklynson31 <mikeharp212@gmail.com>
2017-06-20 13:56:22 -0400

    for loop

---
+++ ControlFlow/forloop.js
@@ -0,0 +1,16 @@
+for(var i = -10; i < 19;i++){
+	console.log(i);
+}
+
+
+for(var j=10; j <40;j++){
+	console.log(j);
+}
+
+for(var k = 300; k<=333;k++){
+	console.log(k);
+}
+
+for(var l = 5; l<=50;l++){
+	console.log(l);
+}
